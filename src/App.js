2d140b4482b98193309fe1c3be38d3a7cb026fef
Eugen Soloviov <suenot@gmail.com>
2016-05-11 18:03:19 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from './components/Counter/Counter';
+import './styles/main.scss';
+
+export default () => (
+  <div className="container">
+    <h1 className="title"> Basic Counter Example</h1>
+    <Counter />
+  </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  }
+};
